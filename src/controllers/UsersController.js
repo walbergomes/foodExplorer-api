32355c93d54fba6b9f32c0fe9167f484b73f7901
Walber <walberg.s48@gmail.com>
2024-08-07 15:37:03 -0300

    :heavy_plus_sign: add AppError - exception handling

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,12 @@
+const AppError = require("../utils/AppError")
+
 class UsersController {
   create(req, res) {
     const { name, email, password } = req.body
+
+    if(!name) {
+      throw new AppError("Nome é obrigatório")
+    }
 
     return res.json({ name, email, password })
   }
--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
 require("dotenv/config")
+require("express-async-errors")
+const AppError = require("./utils/AppError")
+
 const express = require("express")
 
 const routes = require("./routes")
@@ -6,6 +9,22 @@ const routes = require("./routes")
 const app = express()
 app.use(express.json())
 app.use(routes)
+
+app.use((error, req, res, next) => {
+  if(error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    })
+  }
+
+  console.log(error)
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  })
+})
 
 const PORT = process.env.PORT || 3333
 app.listen(PORT, () => {
