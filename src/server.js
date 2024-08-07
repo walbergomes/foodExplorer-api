57d948a633b9ec009fb04acbd484c5e5f2446f0d
Walber <walberg.s48@gmail.com>
2024-08-07 16:55:16 -0300

    :heavy_plus_sign: add sqlite and sqlite3 dependency

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ require("dotenv/config")
 require("express-async-errors")
 const AppError = require("./utils/AppError")
 
+const database = require("./database/sqlite")
+
 const express = require("express")
 
 const routes = require("./routes")
@@ -9,6 +11,8 @@ const routes = require("./routes")
 const app = express()
 app.use(express.json())
 app.use(routes)
+
+database()
 
 app.use((error, req, res, next) => {
   if(error instanceof AppError) {
