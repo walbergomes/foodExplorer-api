b2b0e8d34d55556be4baba3868a27828f4dc8a5d
Walber <walberg.s48@gmail.com>
2024-10-22 23:02:44 -0300

    :heavy_plus_sign: add knex  dependency

---
+++ knexfile.js
@@ -0,0 +1,17 @@
+const path = require("path")
+
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(__dirname, "stc", "database", "database.db")
+    },
+    pool: {
+      afterCreate: (connection, callBack) => connection.run("PRAGMA foreign_keys = ON", callBack)
+    },
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+    },
+    useNullAsDefault: true
+  },
+};
