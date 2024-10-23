c275bf68d101267ed99670c4f3b7a4e2627138fe
Walber <walberg.s48@gmail.com>
2024-10-22 22:56:15 -0300

    :heavy_plus_sign: add sqlite dependency

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 const express = require("express")
+const database = require("./database/sqlite")
 
 const app = express()
+
 
 app.get("/", (req, res) => {
   return res.json("Hello, world!")
 })
 
+database()
 
 const PORT = 3333
 app.listen(PORT, () => {
