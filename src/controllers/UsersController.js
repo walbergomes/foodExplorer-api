b270ca6a422dc8bb0fee271eefbb6b4d6105fef7
Walber <walberg.s48@gmail.com>
2024-10-25 21:53:17 -0300

    :sparkles: add create method - UsersController

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,26 @@
 const AppError = require("../utils/AppError");
+const knex = require("../database/knex")
+const { hash } = require("bcryptjs")
 
 class UsersController {
-  create(req, res) {
+  async create(req, res) {
     const { name, email, password } = req.body;
 
-    if (!name) {
-      throw new AppError("Nome é obrigatório");
+    const emailInUse = await knex("users").where({ email }).first()
+
+    if(emailInUse) {
+      throw new AppError("Esse email já está em uso!")
     }
 
-    return res.json({ email, password });
+    const passwordHashed = await hash(password, 8)
+
+    await knex("users").insert({
+      name, 
+      email,
+      password: passwordHashed
+    })
+
+    return res.status(201).json(emailInUse);
   }
 }
 
